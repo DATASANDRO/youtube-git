98514a41a5320cbe6699bc687f8a2df15ec09910
lola y trista <patriciapato3194@gmail.com>
2024-03-29 13:11:00 -0500

    puse un log en js

--- javascritp.js
+++ javascritp.js
@@ -7,3 +7,4 @@ console.log("aagregando dato desde master y actualizando otra vez")
 
 console.log("agregando un dato adicional en mi  js desde js y analizando")
 
+console.log("hola estoy asiendo esto cabeson")
